dcc82b1421ed5345366ea56be74aa59e7a4ee6c9
arslan-codes <arslanmazharnfts@gmail.com>
2024-10-05 16:00:49 +0500

    user routes

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const JWT_secret = "sbkjhasd";
 
 app.use(express.json());
 // user signup and sign in
-app.post("/signup", (req, res) => {});
-app.post("/signin", (req, res) => {});
-app.post("/purchase", (req, res) => {});
+app.post("/user/signup", (req, res) => {});
+app.post("/user/signin", (req, res) => {});
+//all my courses
+app.get("/user/purchases", (req, res) => {});
+//to view all the courses
 app.get("/courses", (req, res) => {});
-app.get("/purchased", (req, res) => {});
+//purchase a course
+app.post("/course/purchase", (req, res) => {});

8dbbb1b833a89ac21eca85e4f6a1ebf83a91de80
arslan-codes <arslanmazharnfts@gmail.com>
2024-10-06 10:53:49 +0500

    updated code

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-// const JWT = require("jsonwebtoken");
-// const JWT_secret = "sbkjhasd";
+const mongoose = require("mongoose");
+
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
+
 app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
-app.use("api/v1/admin", adminRouter);
+app.use("/api/v1/admin", adminRouter);
 
-app.listen(3000, () => {
-  console.log("app is listtening to port 3000");
-});
+async function main() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB URL:", process.env.MONGO_URL);
+
+    console.log("Connected to MongoDB");
+
+    app.listen(3000, () => {
+      console.log("App is listening to port 3000");
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+}
+
+main();
